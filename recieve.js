c5b60def9da6d0eeb7762050404ef753d5e401d4
manuel897 <55998830+manuel897@users.noreply.github.com>
2019-10-15 18:37:30 +0200

    Add files via upload

---
+++ recieve.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var queue1 = 'firstQ';
+        var queue2 = 'secondQ';
+
+        channel.assertQueue(queue1, {
+            durable: false
+        });
+        channel.assertQueue(queue2, {
+            durable: false
+        }); 
+
+        console.log(" [*] Waiting for messages in %s and %ss. To exit press CTRL+C", queue1 , queue2);
+        
+        channel.consume(queue1, function(msg) {
+            console.log(" [x] Received %s", msg.content.toString());
+           // channel.ack(msg);
+        }, {
+            
+            noAck: false
+        });
+
+        channel.consume(queue2, function(msg) {
+            console.log(" [x] Received %s", msg.content.toString());
+          //  channel.ack(msg);
+        }, {
+            noAck: false
+        }); 
+    });
+});
---
+++ send2.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+// DOC: https://www.rabbitmq.com/tutorials/tutorial-one-javascript.html
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var min=25; 
+        var max=28;  
+        var random  = Math.random() ;
+
+
+
+        var queue = 'secondQ';
+        var msg = random.toString();
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        setInterval( () => {
+            var random  =  Math.floor(Math.random() * (+max - +min)) + +min; 
+            var msg = random.toString()+" °C from Indoor";
+            channel.sendToQueue(queue, Buffer.from(msg)); 
+            console.log(" [x] Sent %s", msg);
+        } 
+        ,10000); 
+
+     
+    });
+    setTimeout(function() {
+        connection.close();
+        process.exit(0);
+    }, 600000);
+    
+});
